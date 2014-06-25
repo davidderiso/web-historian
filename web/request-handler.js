9fb7992aac175e8679c5b2e216719753794be23a
Hack Reactor Students <davidderiso@gmail.com>
2014-06-25 15:19:30 -0700

    Route to index and serve static HTML

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,15 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers.js');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.url === '/' || req.url === '/index.html') {
+    httpHelpers.serveAssets(res, './public/index.html');
+  }
+  else {
+   res.writeHead(404);
+   res.end('Not Found :(');
+  }
 };
